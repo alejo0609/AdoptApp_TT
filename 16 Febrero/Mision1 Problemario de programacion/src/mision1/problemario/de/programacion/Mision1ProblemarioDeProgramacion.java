998c1ced36fb7ee9d55f50fcca8e3f8eac269ab4
alejo0609 <alejandro.perez9@udea.edu.co>
2025-02-22 00:24:48 -0500

    first commit

--- 16 Febrero/Mision1 Problemario de programacion/src/mision1/problemario/de/programacion/Mision1ProblemarioDeProgramacion.java
+++ 16 Febrero/Mision1 Problemario de programacion/src/mision1/problemario/de/programacion/Mision1ProblemarioDeProgramacion.java
@@ -10,20 +10,23 @@ public class Mision1ProblemarioDeProgramacion {
         float num1, num2, num3; // Declaramos las variables, pero las inicializamos dentro de cada caso
 
         do {
-            System.out.println("\nMenú de Ejercicios");
+            System.out.println("\nMenú de Ejercicios\n");
             System.out.println( "1. Determinar si un número es par o impar\n"
                     +           "2. Determinar si un número entero es positivo o negativo\n"
                     +           "3. Realizar suma, resta y multiplicación de dos números\n"
                     +           "4. Calcular el mayor de los números ingresados\n"
-                    +           "5. Calcular el mayor de los números ingresados\n"
+                    +           "5. Calcular el mayor de 3 números enteros ingresados\n"
+                    +           "6. Calcular el mayor de 4 números enteros ingresados\n"
+                    +           "7. Calcular el estado del agua según su temperatura\n" 
+                    +           "8. Calcular si un año es bisiesto o no\n\n"
                     
-                    +           "0. Salir\n"
-                    +           "Seleccione una opción: ");
+                    +           "0. Salir\n\n\n"
+                    +           "Seleccione una opción: \n");
             opcion = scanner.nextInt();
 
             switch (opcion) {
                 case 1:
-                    System.out.print("Ingrese un número: ");
+                    System.out.println("Ingrese un número: ");
                     num1 = scanner.nextInt();
                     if (num1 % 2 == 0) {
                         System.out.println("El número " + num1 + " es PAR.");
@@ -66,26 +69,83 @@ public class Mision1ProblemarioDeProgramacion {
                     System.out.print("Ingrese el segundo número: ");
                     num2 = scanner.nextFloat();
                     if(num1>num2){
-                        System.out.print("El número " + num1 + " es MAYOR que " + num2 );
+                        System.out.println("El número " + num1 + " es MAYOR que " + num2 );
                     }
                     else{
-                        System.out.print("El número " + num1 + " es MENOR que " + num2 );
+                        System.out.println("El número " + num1 + " es MENOR que " + num2 );
                     }
                     break;
                 
                 case 5: // Calcular el mayor de 3 números
                     int mayor= Integer.MIN_VALUE;
                     for (int i=1; i<=3; i++){
-                       System.out.print("Ingrese el número: " + i + ": "); 
+                       System.out.println("Ingrese el número: " + i + ": "); 
                        int num = scanner.nextInt();
                        if(num>mayor){
                            mayor=num;
                        }
                     }
-                    System.out.print("El número mayor de los tres ingresados es el: " + mayor); 
+                    System.out.println("\n\nEl número mayor de los tres ingresados es el: " + mayor); 
+                    
+                    break;
+                    
+                case 6: // Calcular el mayor de 4 números
+                    int mayor2= Integer.MIN_VALUE;
+                    for (int i=1; i<=4; i++){
+                       System.out.print("Ingrese el número: " + i + ": "); 
+                       int num = scanner.nextInt();
+                       if(num>mayor2){
+                           mayor2=num;
+                       }
+                    }
+                    System.out.println("\n\nEl número mayor de los cuatro ingresados es el: " + mayor2); 
+                    
+                    break;
+                    
+                case 7: // Calcula el estado del agua de acuerdo a la temperatura
+                    //int mayor3= Integer.MIN_VALUE;
+                    String estado="";
+                    System.out.print("Ingrese el el valor de la temperatura: "); 
+                    double num = scanner.nextInt();
+                    if(num<0){
+                        estado="Sólido";
+                    }
+                    else if(num>0 && num<=100){
+                        estado="Líquido";
+                    }
+                    else{
+                        estado="Gas";
+                    } 
+                    System.out.println("\n\nEl estado actual del agua es: " + estado); 
                     
                     break;
 
+                 case 8: // Calcula un año Bisiestos
+                    int residuo;
+                    System.out.print("Ingrese el año para determinar si es bisiesto o no: "); 
+                    int num4 = scanner.nextInt();
+                    residuo = num4%4;
+                    if(residuo == 0){
+                        if(num4%100 == 0){
+                            if(num4%400 == 0){
+                                System.out.println("\n\nEl año: " + num4 + " es Bisiesto");
+                                break;
+                            }
+                            else{
+                                System.out.println("\n\nEl año: " + num4 + " NO es Bisiesto");
+                                break;
+                            }
+                        }
+                        System.out.println("\n\nEl año: " + num4 + " es Bisiesto");
+                        
+                    }
+                    else{
+                        System.out.println("\n\nEl año: " + num4 + " NO es Bisiesto");
+                    }
+                    
+                    break;                   
+                    
+                    
                 case 0:
                     System.out.println("Saliendo del programa...");
                     break;
@@ -98,5 +158,3 @@ public class Mision1ProblemarioDeProgramacion {
         scanner.close();
     }
 }
-3
-3
