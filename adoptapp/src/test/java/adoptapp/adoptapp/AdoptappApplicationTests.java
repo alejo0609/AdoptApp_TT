82e0f55c07bee4ca7c175be4984ddbef5635ed8a
TuNuevoUsuario <tuemail@nuevogit.com>
2025-03-15 11:25:13 -0500

    Ensayo de primer rama

---
+++ adoptapp/src/test/java/adoptapp/adoptapp/AdoptappApplicationTests.java
@@ -0,0 +1,13 @@
+package adoptapp.adoptapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
