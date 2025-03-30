40d733ec42456a89d7709f843730950ff3f38f0b
TuNuevoUsuario <tuemail@nuevogit.com>
2025-03-30 12:45:48 -0500

    BD 12:45 pm

--- animalitos.sql
+++ animalitos.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `animalitos` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `animalitos`;
 -- MySQL dump 10.13  Distrib 8.0.13, for macos10.14 (x86_64)
 --
 -- Host: 127.0.0.1    Database: animalitos
@@ -307,4 +309,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-03-30 12:28:45
+-- Dump completed on 2025-03-30 12:45:18
