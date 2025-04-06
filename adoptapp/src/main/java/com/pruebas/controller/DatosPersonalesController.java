ce9c091a48442257cbc6883099f1b298b92cb69b
TuNuevoUsuario <tuemail@nuevogit.com>
2025-04-06 10:17:07 -0500

    FINAL

--- adoptapp/src/main/java/com/pruebas/controller/DatosPersonalesController.java
+++ adoptapp/src/main/java/com/pruebas/controller/DatosPersonalesController.java
@@ -1,3 +1,10 @@
+ /** 
+ * @author Alejandro Perez CC 8.029.742
+ * @author Julian David Giraldo Murillo CC 1.007.240.094
+ * @author Andres Escobar Vasquez CC 1.038.096.962
+ * @author Jorge Andres Restrepo Cataño CC 98.648.720
+ **/
+ 
 package com.pruebas.controller;
 
 import com.pruebas.model.DatosPersonalesModel;
--- adoptapp/src/main/java/com/pruebas/service/TipoVacunaService.java
+++ adoptapp/src/main/java/com/pruebas/service/TipoVacunaService.java
@@ -1,3 +1,10 @@
+ /** 
+ * @author Alejandro Perez CC 8.029.742
+ * @author Julian David Giraldo Murillo CC 1.007.240.094
+ * @author Andres Escobar Vasquez CC 1.038.096.962
+ * @author Jorge Andres Restrepo Cataño CC 98.648.720
+ **/
+ 
 package com.pruebas.service;
 
 import com.pruebas.model.TipoVacunaModel;
