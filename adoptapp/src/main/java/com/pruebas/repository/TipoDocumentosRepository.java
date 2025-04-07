39db91f8069dacec32a9c3ad8e1206e03ce1f18c
TuNuevoUsuario <tuemail@nuevogit.com>
2025-04-06 21:42:34 -0500

    Alejo

--- adoptapp/src/main/java/com/pruebas/repository/TipoDocumentosRepository.java
+++ adoptapp/src/main/java/com/pruebas/repository/TipoDocumentosRepository.java
@@ -1,5 +1,5 @@
  /** 
- * @author Alejandro Perez CC 8.029.742
+ * @author Alejandro Perez R CC 8.029.742
  * @author Julian David Giraldo Murillo CC 1.007.240.094
  * @author Andres Escobar Vasquez CC 1.038.096.962
  * @author Jorge Andres Restrepo Cataño CC 98.648.720
